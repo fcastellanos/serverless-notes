1b361de243b3f51c209ef34db4189e0891fc50af
Fernando Castellanos <fernando.castellanos@gmail.com>
2019-02-04 19:49:53 -0800

    Creating the web app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "bit-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://jbe612cccb.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ICegitJeJ",
+    APP_CLIENT_ID: "1hu709ct4p0b31476657diid6s",
+    IDENTITY_POOL_ID: "us-east-1:9c990d72-bfde-46ec-93e3-627cc465faac"
+  }
+};
