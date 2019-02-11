ae9b4161880f297808f88286d2d384a3efa7bde9
Fernando Castellanos <fcastellanos@bandsintown.com>
2019-02-11 11:02:11 -0800

    Add settings page

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -1,17 +1,45 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+// TODO: Need to update values
+const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "bit-notes-uploads"
+    BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://jbe612cccb.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "YOUR_DEV_API_GATEWAY_URL"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_ICegitJeJ",
-    APP_CLIENT_ID: "1hu709ct4p0b31476657diid6s",
-    IDENTITY_POOL_ID: "us-east-1:9c990d72-bfde-46ec-93e3-627cc465faac"
+    USER_POOL_ID: "YOUR_DEV_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_DEV_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_DEV_IDENTITY_POOL_ID"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "YOUR_PROD_API_GATEWAY_URL"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
